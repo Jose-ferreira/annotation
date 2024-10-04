877bfc5059e37a9f6a5660ce7f0e22af30a2b8dd
Jose Ferreira <jose.ferreira@live.co.uk>
2024-10-05 00:46:58 +0100

    repeatable annotations working

--- processor/src/main/java/org/example/PcdpListenerProcessor.java
+++ processor/src/main/java/org/example/PcdpListenerProcessor.java
@@ -18,26 +18,8 @@ public class PcdpListenerProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 
-        var tmp = roundEnv.getElementsAnnotatedWith(PcdpListener.class);
-        print(tmp);
-
-        /*for (Element element : roundEnv.getElementsAnnotatedWith(PcdpListener.class)) {
-            print(element);
-            var tmp2 = element.getAnnotationsByType(PcdpListener.class).length;
-            print(tmp2);
-        }*/
-
-        //processPcdpListeners(roundEnv.getElementsAnnotatedWith(PcdpListeners.class));
-
-
-        print("single");
         processPcdpListener(roundEnv.getElementsAnnotatedWith(PcdpListener.class));
-        print("Multiple");
         processPcdpListeners(roundEnv.getElementsAnnotatedWith(PcdpListeners.class));
-
-        //processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Jose");
-
-
         return false;
     }
 
@@ -53,53 +35,34 @@ public class PcdpListenerProcessor extends AbstractProcessor {
 
     private void processPcdpListeners(Set<? extends Element> pcdpListenersElements){
         for (Element element : pcdpListenersElements) {
-
+            String packageName = ((PackageElement)element.getEnclosingElement()).getQualifiedName().toString();
             for (PcdpListeners pcdpListeners : element.getAnnotationsByType(PcdpListeners.class)) {
-                for (PcdpListener pcdpListener :pcdpListeners.value()) {
-
-                    String packageName = ((PackageElement)element.getEnclosingElement()).getQualifiedName().toString();
-                    String context = pcdpListener.context();
-                    String className = element.getSimpleName().toString() + context + "Listener";
-                    print(className);
-
-                    generateClass(className, packageName);
-                }
+                generatePcdpListener(pcdpListeners.value(), packageName);
             }
         }
     }
 
     private void processPcdpListener(Set<? extends Element> pcdpListenerElements){
         for (Element element : pcdpListenerElements) {
-
             String packageName = ((PackageElement)element.getEnclosingElement()).getQualifiedName().toString();
-            String context = element.getAnnotation(PcdpListener.class).context();
-            String className = element.getSimpleName().toString() + context + "Listener";
-
-            generateClass(className, packageName);
+            generatePcdpListener(element.getAnnotationsByType(PcdpListener.class), packageName);
         }
     }
 
-    private void generateClass(String className, String packageName){
-
-        try {
-            JavaFileObject generatedClass = processingEnv.getFiler().createSourceFile(className);
-            BufferedWriter bufferedWriter = new BufferedWriter(generatedClass.openWriter());
-            bufferedWriter.append("package " + packageName + ";");
-            bufferedWriter.newLine();
-            bufferedWriter.append("public class ");
-            bufferedWriter.append(className);
-            bufferedWriter.append("{");
-            bufferedWriter.newLine();
-            bufferedWriter.append("////TODO Flesh out litener");
-            bufferedWriter.newLine();
-            bufferedWriter.append("}");
-            bufferedWriter.close();
-        } catch (IOException e) {
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, e.toString());
+    private void generatePcdpListener(PcdpListener[] pcdpListeners, String packageName){
+        for (PcdpListener pcdpListener : pcdpListeners) {
+            String className = pcdpListener.context() + "Listener";
+            try {
+                JavaFileObject generatedClass = processingEnv.getFiler().createSourceFile(className);
+                BufferedWriter bufferedWriter = new BufferedWriter(generatedClass.openWriter());
+                bufferedWriter.append("package " + packageName + ";\n");
+                bufferedWriter.append("public class " + className + "{\n");
+                bufferedWriter.append("////TODO Flesh out listener\n");
+                bufferedWriter.append("}");
+                bufferedWriter.close();
+            } catch (IOException e) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, e.toString());
+            }
         }
-    }
-
-    private void print(Object object){
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, String.valueOf(object));
     }
 }
